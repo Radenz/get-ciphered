19ece900e5f9d934ea4929dc81c191f8315a5347
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-01-25 22:03:12 +0700

    add svelte template

---
+++ cipher-app/src/App.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1>Cipherin Aja!</h1>
+	<input type="textbox">
+	<p>Or</p>
+	<input type="file">
+	<div>
+		<input type="button" value="encrypt">
+		<input type="button" value="decrypt">
+	</div>
+	
+	<input type="textbox" disabled>
+	<p>Or</p>
+	<button >download</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
