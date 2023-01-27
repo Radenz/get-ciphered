fd571dfe6c13a19f13bfd311ca9cbd28b3a42e95
Radenz <radenrifqirahman@gmail.com>
2023-01-27 09:23:25 +0700

    feat: implement affine cipher

---
+++ src/lib/cipher/affine.ts
@@ -0,0 +1,47 @@
+import { alphaCodeOf, alphaUpperCaseOf } from "./utils/char";
+import { inverseMod, mod } from "./utils/math";
+
+class AffineCipher {
+  private inverseModulus: number;
+
+  constructor(
+    private readonly multiplier: number,
+    private readonly offset: number
+  ) {
+    this.inverseModulus = inverseMod(multiplier, 1, 26);
+
+    console.log(this.inverseModulus);
+  }
+
+  encrypt(source: string): string {
+    const encrypted = [];
+
+    for (let i = 0; i < source.length; i++) {
+      const char = source.charCodeAt(i);
+      const newAlphaCode = mod(
+        alphaCodeOf(char) * this.multiplier + this.offset,
+        26
+      );
+      encrypted.push(alphaUpperCaseOf(newAlphaCode));
+    }
+
+    return String.fromCharCode(...encrypted);
+  }
+
+  decrypt(encrypted: string): string {
+    const source = [];
+
+    for (let i = 0; i < encrypted.length; i++) {
+      const char = encrypted.charCodeAt(i);
+      const newAlphaCode = mod(
+        (alphaCodeOf(char) - this.offset) * this.inverseModulus,
+        26
+      );
+      source.push(alphaUpperCaseOf(newAlphaCode));
+    }
+
+    return String.fromCharCode(...source);
+  }
+}
+
+export { AffineCipher };
--- src/lib/cipher/utils/math.ts
+++ src/lib/cipher/utils/math.ts
@@ -2,4 +2,32 @@ function mod(a: number, b: number) {
   return ((a % b) + b) % b;
 }
 
-export { mod };
+function gcd(a: number, b: number) {
+  if (b > a) return gcd(b, a);
+
+  return a % b == 0 ? b : gcd(b, a % b);
+}
+
+function inverseMod(multiplier: number, remainder: number, modulus: number) {
+  const modulusGcd = gcd(multiplier, modulus);
+
+  if (remainder % multiplier == 0) {
+    return remainder / multiplier;
+  }
+
+  multiplier /= modulusGcd;
+  modulus /= modulusGcd;
+  remainder /= modulusGcd;
+
+  multiplier = mod(multiplier, modulus);
+  remainder = mod(remainder, modulus);
+
+  const newMultiplier = modulus % multiplier;
+  const newModulus = multiplier;
+  const newRemainder = mod(-remainder, multiplier);
+  const addition = inverseMod(newMultiplier, newRemainder, newModulus);
+  const inverse = (addition * modulus + remainder) / multiplier;
+  return mod(inverse, modulus);
+}
+
+export { mod, gcd, inverseMod };
