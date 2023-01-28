bf8dcd7827f5039b1c46d1644f7ca8445d85f5e5
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2023-01-28 14:37:21 +0700

    feat: UI Affine cipher and Hillcipher

---
+++ src/AffinePage.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Tab, TabGroup } from "@skeletonlabs/skeleton";
+  import { writable } from "svelte/store";
+  // import FileInputPlayfairCipher from "./lib/components/cipher/FileInputPlayfairCipher.svelte";
+  import TextInputPlayfairCipher from "./lib/components/cipher/TextInputAffineCipher.svelte";
+  // import {  } from "./lib/cipher/playfair";
+
+  let input = writable("text");
+  const component = {
+    text: TextInputPlayfairCipher,
+    // file: FileInputPlayfairCipher,
+  };
+</script>
+
+<div class="flex flex-col gap-4 h-full">
+  <TabGroup selected={input} justify="justify-center">
+    <Tab value="text">Text Input</Tab>
+    <Tab value="file">File Input</Tab>
+  </TabGroup>
+  <svelte:component this={component[$input]} />
+</div>
---
+++ src/lib/components/cipher/TextInputHillCipher.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+  import { HillCipher } from "../../cipher/hill";
+  import { chunked } from "../../cipher/utils/char";
+  import { saveText } from "../../utils/save";
+  import { ModulusMatrix } from "../../cipher/utils/math";
+
+  export let cipher: HillCipher;
+
+  let source: string;
+  let result: string = "";
+  let key: string;
+  let keymatrix: ModulusMatrix;
+
+  function createMatrixKey(){
+    const arr = key.split(" ")
+    const size = Math.sqrt(arr.length)
+    let idx =0;
+    keymatrix = new ModulusMatrix(size,size);
+    for (let i = 0; i < size; i++){
+      for(let j = 0; j < size; j++){
+        keymatrix.set(i,j,Number(arr[idx]));
+        idx++;
+      }
+    }
+
+  }
+  function encrypt() {
+    createMatrixKey();
+    console.log(keymatrix);
+    // cipher = new HillCipher(keymatrix);
+    // result = cipher.encrypt(source);
+  }
+
+  function decrypt() {
+    createMatrixKey();
+    cipher = new HillCipher(keymatrix);
+    result = cipher.decrypt(source);
+  }
+
+  function compact() {
+    result = result.replaceAll(" ", "");
+  }
+
+  function chunk() {
+    if (result.includes(" ")) return;
+    const chunks = chunked(result, 5);
+    result = chunks.join(" ");
+  }
+</script>
+
+<div class="grow grid grid-rows-[1fr_auto] gap-4 h-full">
+  <div class="grid grid-cols-[1fr_1fr] gap-6">
+    <div class="h-full">
+      <label class="input-label box-border grid grid-rows-[auto_1fr] h-full">
+        <h4>Input</h4>
+        <textarea bind:value={source} class="resize-none" />
+      </label>
+    </div>
+    <div class="input-label h-full box-border grid grid-rows-[auto_1fr]">
+      <h4>Result</h4>
+      <div
+        class="bg-surface-700 rounded-md border-surface-500 border box-border p-2"
+      >
+        {result}
+      </div>
+    </div>
+  </div>
+  <div class="grid grid-cols-[1fr_1fr] gap-6">
+    <div class="flex items-center justify-between gap-6">
+      <label class="input-label box-border flex items-center gap-4 grow">
+        <h4>KeyMatrix</h4>
+        <input type="text" bind:value={key} class="h-8 text-input" />
+      </label>
+      <div class="flex gap-4">
+        <button
+          class="btn btn-sm variant-filled-primary font-label font-semibold"
+          on:click={encrypt}
+        >
+          Encrypt
+        </button>
+        <button
+          class="btn btn-sm variant-filled-primary font-label font-semibold"
+          on:click={decrypt}
+        >
+          Decrypt
+        </button>
+      </div>
+    </div>
+    <div class="flex justify-between items-center">
+      <div class="flex gap-4 items-center">
+        <h4>Format:</h4>
+        <button
+          class="btn btn-sm variant-filled-primary font-label font-semibold"
+          on:click={compact}
+        >
+          Compact
+        </button>
+        <button
+          class="btn btn-sm variant-filled-primary font-label font-semibold"
+          on:click={chunk}
+        >
+          Chunked
+        </button>
+      </div>
+      <div>
+        <button
+          class="btn btn-sm variant-filled-primary font-label font-semibold"
+          on:click={() => {
+            saveText(result, "encrypted.txt");
+          }}
+        >
+          Download
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .text-input {
+    margin-top: 0 !important;
+  }
+</style>
